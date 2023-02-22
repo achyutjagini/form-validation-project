6980b2e5885bfa9a512b3023c78a952b12e98d93
achyutjagini <achyut.jagini@gmail.com>
2023-02-22 18:05:04 +0530

    3 files modified

--- index.js
+++ index.js
@@ -4,17 +4,3 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 //appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
-function validate() {
-  let s = document.createElement('p');
-  let x = document.getElementsByClassName('names');
-
-  for (let i = 0; i < x.length; i++) {
-    if (x[i].value.length < 3 || x[i].value.length > 16) {
-      s = document.getElementsByClassName(x[i].value);
-      s.innerText = 'error';
-      alert('D');
-      return false;
-    }
-  }
-}
