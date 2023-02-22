e3980c6beffbbaf30bb97e3c47391c5ef0ffdec5
achyutjagini <achyut.jagini@gmail.com>
2023-02-22 12:49:03 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,5 +5,17 @@ import './style.css';
 const appDiv = document.getElementById('app');
 //appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-let s = document.createElement('p');
-x=do
+function validate() {
+  let s = document.createElement('p');
+  let x = document.getElementsByClassName('names');
+
+  for (let i = 0; i < x.length; i++) {
+    if (x[i].value.length < 3 || x[i].value.length > 16) {
+      let s = document.createElement('p');
+      s.textContent = 'invalid input';
+      x[i].appendChild(s);
+      //alert('D');
+      return false;
+    }
+  }
+}
